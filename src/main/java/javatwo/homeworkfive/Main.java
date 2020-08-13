87fead2a7b4ca34badc76ba77a9e3917605bee0d
Arthur <ergo.90@gmail.com>
2020-08-13 18:59:00 +0300

    finish homework five

---
+++ src/main/java/javatwo/homeworkfive/Main.java
@@ -0,0 +1,80 @@
+package javatwo.homeworkfive;
+
+import java.util.Arrays;
+
+public class Main {
+    static final int ARRAY_SIZE = 10_000_000;
+    static final int ARRAY_HALF_SIZE = ARRAY_SIZE / 2;
+    float[] arrayField;
+
+    public Main(){
+        this.arrayField = new float[ARRAY_SIZE];
+        Arrays.fill(this.arrayField, 1.0f);
+    }
+
+
+    public void assignNewValue(float[] array){
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (float) (array[i] * Math.sin(0.2f + i / 5.f) * Math.cos(0.2f + i / 5.f) * Math.cos(0.4f + i / 2.f));
+        }
+    }
+    public long assignNewValueOneThread(){
+        long timeStart = System.currentTimeMillis();
+        this.assignNewValue(this.arrayField);
+        return System.currentTimeMillis() - timeStart;
+    }
+
+    public long assignNewValueTwoThreads(){
+        long timeStart = System.currentTimeMillis();
+        float[] arrayPartOne = new float[ARRAY_HALF_SIZE];
+        float[] arrayPartTwo = new float[ARRAY_HALF_SIZE];
+        System.arraycopy(this.arrayField, 0, arrayPartOne, 0, arrayPartOne.length);
+        System.arraycopy(this.arrayField, arrayPartOne.length, arrayPartTwo, 0, arrayPartTwo.length);
+        Thread thread1 = new Thread(() -> this.assignNewValue(arrayPartOne));
+        Thread thread2 = new Thread(() -> this.assignNewValue(arrayPartTwo));
+        thread1.start();
+        thread2.start();
+        try{
+            thread1.join();
+            thread2.join();
+
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        }
+
+        System.arraycopy(arrayPartOne, 0, this.arrayField, 0, arrayPartOne.length);
+        System.arraycopy(arrayPartTwo, 0, this.arrayField, arrayPartOne.length, arrayPartTwo.length);
+        return System.currentTimeMillis() - timeStart;
+    }
+
+    public void showFirstAndLastTenArrayValues(){
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < 10; i++) {
+            result.append(this.arrayField[i]).append(", ");
+        }
+        for (int i = this.arrayField.length-10; i < this.arrayField.length; i++) {
+            result.append(this.arrayField[i]).append(", ");
+        }
+        System.out.println(result);
+
+    }
+
+    public static void main(String[] args) {
+        Main array1 = new Main();
+        Main array2 = new Main();
+
+        array1.showFirstAndLastTenArrayValues();
+        System.out.println();
+        System.out.println(array1.assignNewValueOneThread());
+        System.out.println();
+        array1.showFirstAndLastTenArrayValues();
+        System.out.println();
+
+        array2.showFirstAndLastTenArrayValues();
+        System.out.println();
+        System.out.println(array2.assignNewValueTwoThreads());
+        System.out.println();
+        array2.showFirstAndLastTenArrayValues();
+        System.out.println();
+    }
+}
